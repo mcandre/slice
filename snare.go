1c2c25fa7960b7d810818519debbcf1699d827a2
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2022-02-23 22:11:46 -0600

    bump to v0.0.5

--- snare.go
+++ snare.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is semver.
-const Version = "0.0.4"
+const Version = "0.0.5"
 
 // DefaultRate controls the normal probability preservation rate of each line.
 const DefaultRate = float64(0.1)
